b51b252b10614a00ffd1f46968fd405f2c2357a7
曽根田悠介 <yusuke.soneda@gmail.com>
2017-07-28 21:41:03 +0900

    add config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+CK = 'C5sC0uYjFYm5bEBofVZZ5G5Ls'                             # Consumer Key
+CS = 'awIZrvo4PR75LZZV4CSLx36oAAPkwXH2mO8BCefurSHHbATOgD'         # Consumer Secret
+AT = '830065384501829633-ZSMtFex0yP51liRWnGe29Lnfn3iVHsA' # Access Token
+AS = '5jvMukksq55De2VEXioAp2aO5wBAEnfGn08oFPunoZCoi'      # Accesss Token Secert
