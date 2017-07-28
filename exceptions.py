3a4e3e92c172e4837cfa1b05098f3db51f964a62
曽根田悠介 <yusuke.soneda@gmail.com>
2017-07-29 01:20:51 +0900

    ツイッターからスクレイピングとエラーが発生した時の例外処理を書きました．

--- exceptions.py
+++ exceptions.py
@@ -4,3 +4,10 @@ class UserNotFoundException(Exception):
 
     def __str__(self):
         return self.message
+
+class UnauthorizedException(Exception):
+    def __init__(self, message=''):
+        self.message = message
+
+    def __str__(self):
+        return self.message
