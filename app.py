65d4bdcb60736a150b7a92c0710b5aabb622f2e0
Wakimoto Ryu <sidebook37@gmail.com>
2017-07-29 18:51:22 +0900

    modified iroiro

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ import profiler
 
 app = Flask(__name__)
 
+HOME_HTML = 'ishinomakihackathon.html'
+
 def picked_up():
     messages = [
         "こんにちは，あなたの名前を入力してください",
@@ -20,7 +22,7 @@ def picked_up():
 @app.route('/', methods=['GET'])
 def index():
     message = picked_up()
-    return render_template("cupid.html",message=message,title=settings.TITLE)
+    return render_template(HOME_HTML, message=message,title=settings.TITLE)
 
 @app.route('/post',methods=['GET','POST'])
 def post():
@@ -31,20 +33,34 @@ def post():
         #print(woman)
 
         compatibility = ''
+        profiled = False
         try:
             profile_man = profiler.from_user_id(man)
             profile_woman = profiler.from_user_id(woman)
             compatibility = str(profile_man.compare(profile_woman) * 100) + ' %'
+            profiled = True
         except UserNotFoundException as e:
             compatibility = 'ユーザーが見つかりません'
         except UnauthorizedException as e:
             compatibility = '非公開アカウントです'
         except InsufficientTweetsError as e:
             compatibility = 'ツイートが少なすぎます'
-
-        return render_template('index.html',name=compatibility,title=settings.TITLE)
+        if profiled:
+            profile = []
+            for k, v in profile_man.personality.items():
+                man_p = '{0:.1f} %'.format(v * 100)
+                woman_p = '{0:.1f} %'.format(profile_woman.personality[k] * 100)
+                profile.append({'name': k, 'man': man_p, 'woman': woman_p})
+            
+            return render_template(
+                HOME_HTML,
+                profiled=True,
+                profile=profile
+            )
+        else:
+            return render_template(HOME_HTML)
     else:
-        return redirect(url_for('ishinomakihackathon.html'))
+        return redirect(url_for(HOME_HTML))
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
